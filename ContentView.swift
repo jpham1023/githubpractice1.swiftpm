32fb891bbc3e189ad795bc9343fd533a44ab4007
Jhhs xcode <phamthaoquyen@gmail.com>
2023-09-21 10:29:00 -0500

    changed array to type task

--- ContentView.swift
+++ ContentView.swift
@@ -1,10 +1,10 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var myarray = ["Mow the lawn", "Feed the dog", "Grocery Shop"]
+    @State var myarray:[Task] = [Task(nameOfTask:"Mow the lawn"), Task(nameOfTask:"Feed the dog"), Task(nameOfTask:"Grocery Shop")]
     var body: some View {
         List(myarray, id: \.self){
-            task in Text(task)
+            task in Text(task.nameOfTask)
         }
     
     }
